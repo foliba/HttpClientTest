7bf4a056153782a53599ff4a1d2269ea2907be14
Roman Alexis Anastasini <root.post@gmail.com>
2018-02-27 00:07:16 +0100

    Add using over all IDisposable objects

--- HTTPClientTest/Program.cs
+++ HTTPClientTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;using System.IO;using System.Net;using System.Net.Http;using System.Threading.Tasks;namespace HTTPClientTest{    /// <summary>    /// This tiny test should showcase the need of try/catch around HttpClient.GetStringAsync    /// </summary>    class Program    {        private const string url = "http://google.com/unknown";        static void Main(string[] args)        {            MainAsync(args).Wait();            Console.WriteLine("\n\n\n=====> end");            Console.ReadKey();        }        static async Task MainAsync(string[] args)        {            Console.WriteLine(url);            try
+﻿using System;using System.Collections.Generic;
+using System.IO;using System.Net;using System.Net.Http;using System.Threading.Tasks;namespace HTTPClientTest{    /// <summary>    /// This tiny test should showcase the need of try/catch around HttpClient.GetStringAsync    /// </summary>    class Program    {        private const string url = "http://google.com/unknown";        static void Main(string[] args)        {            MainAsync(args).Wait();            Console.WriteLine("\n\n\n=====> end");            Console.ReadKey();        }        static async Task MainAsync(string[] args)        {            Console.WriteLine(url);                        try
             {
                 PrintResponse(await MakeHttpCallAsync_Safe());
             }            catch (Exception ex)
@@ -50,28 +51,75 @@
         }
 
 
-        #region using HttpClient.GetStringAsync        private static async Task<string> MakeHttpCallAsync_Unsafe()        {            Console.WriteLine("Making HttpClient.GetStringAsync unsafe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                var responseHtml = await client.GetStringAsync(url);                ret = responseHtml;            }            Console.WriteLine(ret);            return ret;        }        private static async Task<string> MakeHttpCallAsync_Safe()        {            Console.WriteLine("Making HttpClient.GetStringAsync safe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                var request = new HttpRequestMessage(HttpMethod.Get, url);                try                {                    var responseHtml = await client.GetStringAsync(url);                    ret = responseHtml;                }                catch (HttpRequestException ex)                {                    PrintEx(ex, true);                }            }            return ret;        }        #endregion //   using HttpClient.GetStringAsync        #region using HttpResponseMessage.Content.ReadAsString        //  should cause an exception        private static async Task<string> MakeHttpCallAsync_HttpRequestMessage_Unsafe()        {            Console.WriteLine("making HttpResponseMessage.Content.ReadAsString unsafe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                var request = new HttpRequestMessage(HttpMethod.Get, url);                var response = await client.SendAsync(request);                var responseHtml = await response.Content.ReadAsStringAsync();                ret = responseHtml;            }            return ret;        }        private static async Task<string> MakeHttpCallAsync_HttpRequestMessage_Safe()        {            Console.WriteLine("making HttpResponseMessage.Content.ReadAsString safe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                var request = new HttpRequestMessage(HttpMethod.Get, url);                try                {                    var response = await client.SendAsync(request);                    var responseHtml = await response.Content.ReadAsStringAsync();                    ret = responseHtml;                }                catch (HttpRequestException ex)                {                    PrintEx(ex, true);                }            }            return ret;        }        #endregion //   using HttpResponseMessage.Content.ReadAsString		        #region WebRequest        private static async Task<string> MakeHttpCall_WebRequest_Unsafe()        {            Console.WriteLine("making WebRequest unsafe call");            var ret = string.Empty;                        var request = WebRequest.CreateHttp(url);            var response = (HttpWebResponse)await request.GetResponseAsync();            var dataStream = response.GetResponseStream();            var reader = new StreamReader(dataStream);            string responseFromServer = reader.ReadToEnd();            ret = responseFromServer;            reader.Close();            dataStream.Close();            response.Close();            return ret;        }        private static async Task<string> MakeHttpCall_WebRequest_Safe()        {            Console.WriteLine("making WebRequest safe call");            var ret = string.Empty;
-            var response = new HttpWebResponse();
-            
+        #region using HttpClient.GetStringAsync        private static async Task<string> MakeHttpCallAsync_Unsafe()        {            Console.WriteLine("Making HttpClient.GetStringAsync unsafe call");            var ret = string.Empty;            using (var client = new HttpClient())            {
+                //  exception should be thrown here
+                var responseHtml = await client.GetStringAsync(url);                ret = responseHtml;            }            Console.WriteLine(ret);            return ret;        }        private static async Task<string> MakeHttpCallAsync_Safe()        {            Console.WriteLine("Making HttpClient.GetStringAsync safe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                var request = new HttpRequestMessage(HttpMethod.Get, url);                try                {
+                    //  exception should be thrown here
+                    var responseHtml = await client.GetStringAsync(url);                    ret = responseHtml;                }                catch (HttpRequestException ex)                {                    PrintEx(ex, true);                }            }            return ret;        }        #endregion //   using HttpClient.GetStringAsync        #region using HttpResponseMessage.Content.ReadAsString        //  should cause an exception        private static async Task<string> MakeHttpCallAsync_HttpRequestMessage_Unsafe()        {            Console.WriteLine("making HttpResponseMessage.Content.ReadAsString unsafe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    //  exception should be thrown here --> but isn't
+                    //  so we don't need a try/catch block here
+                    var response = await client.SendAsync(request);                    var responseHtml = await response.Content.ReadAsStringAsync();                    ret = responseHtml;
+                }            }            return ret;        }        private static async Task<string> MakeHttpCallAsync_HttpRequestMessage_Safe()        {            Console.WriteLine("making HttpResponseMessage.Content.ReadAsString safe call");            var ret = string.Empty;            using (var client = new HttpClient())            {                using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    try
+                    {
+                        //  exception should be thrown here --> but isn't
+                        //  so we can safely leave remove this try/catch block
+                        var response = await client.SendAsync(request);
+                        var responseHtml = await response.Content.ReadAsStringAsync();
+
+                        ret = responseHtml;
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        PrintEx(ex, true);
+                    }
+                }                }            return ret;        }        #endregion //   using HttpResponseMessage.Content.ReadAsString		        #region WebRequest        private static async Task<string> MakeHttpCall_WebRequest_Unsafe()        {            Console.WriteLine("making WebRequest unsafe call");            var ret = string.Empty;                        var request = WebRequest.CreateHttp(url);
+            //  exception should be thrown here
+            using (var response = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                using (var dataStream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(dataStream))
+                    {
+                        string responseFromServer = reader.ReadToEnd();
+
+                        ret = responseFromServer;
+
+                        reader.Close();
+                    }
+                    dataStream.Close();
+                }
+                response.Close();
+            }
+            return ret;        }        private static async Task<string> MakeHttpCall_WebRequest_Safe()        {            Console.WriteLine("making WebRequest safe call");            var ret = string.Empty;
+
             var request = WebRequest.CreateHttp(url);            try
             {
-                response = (HttpWebResponse)await request.GetResponseAsync();
-                var dataStream = response.GetResponseStream();
-                var reader = new StreamReader(dataStream);
-                string responseFromServer = reader.ReadToEnd();
+                //  exception should be thrown here
+                using (var response = (HttpWebResponse)await request.GetResponseAsync())
+                {
+                    using (var dataStream = response.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(dataStream))
+                        {
+                            string responseFromServer = reader.ReadToEnd();
 
-                ret = responseFromServer;
+                            ret = responseFromServer;
 
-                reader.Close();
-                dataStream.Close();
-                response.Close();
-
-                return ret;
-            }            catch (Exception ex)
+                            reader.Close();
+                        }
+                        dataStream.Close();
+                    }
+                    response.Close();
+                }
+            }
+            catch (WebException ex)
             {
                 PrintEx(ex, true);
-            }            return ret;        }
-        #endregion //   WebRequest        private static void PrintWeDied()
+            }            return ret;        }        #endregion //   WebRequest
+        #region helper        private static void PrintWeDied()
         {
             var color = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
@@ -92,4 +140,5 @@
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(response);
             Console.ForegroundColor = color;
-        }    }}
+        }
+        #endregion //   helper    }}
